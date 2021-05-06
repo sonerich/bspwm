60a3c40b740743b974a9c97da8dc653a7857a35a
Son Erich <82708310+sonerich@users.noreply.github.com>
2021-05-06 18:35:30 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-# replace ~/.zprofile and ~/.zshenv
-rm ~/.zprofile
-rm ~/.zshenv
-ln -s ~/.config/zsh/.zprofile ~/.zprofile
-ln -s ~/.config/zsh/.zshenv ~/.zshenv
-
-# install starship prompt
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
